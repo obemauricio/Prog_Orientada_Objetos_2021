ad8ac9fc4c0a286d520f518b7fed801e7e39e5a5
Obe Mauricio <mauricioobey@gmail.com>
2021-03-09 19:17:14 -0400

    example,error)

--- decorador.py
+++ decorador.py
@@ -11,15 +11,59 @@ def zumbido():
 zumbido = funcion_decoradora(zumbido) """
 
 #Clases whitout getters and setters
-class Millas:
+""" class Millas:
     def __init__(self, distancia = 0):
         self.distancia = distancia
 
     def convertir_a_kilometros(self):
         return (self.distancia * 1.609344)
 
-avion = Millas()
-avion.distancia = 200
-print(avion.distancia)
+#Metodo getter
+def obtener_distancia(self):
+    return self._distancia 
 
-print(avion.convertir_a_kilometros())
+#Metodo Setter
+def definir_distancia(self, valor):
+    if valor < 0:
+        raise ValueError('No es posible convertir distancias menores a 0')
+    self.distancia = valor
+
+ """
+
+class Millas:
+    def __init__(self):
+        self._distancia = 0
+    
+    #getter
+    @property
+    def distancia(self):
+        print('Llamando a guetter')
+        print(self.distancia)
+        return self._distancia
+
+    #setter
+    @distancia.setter
+    def distancia(self, valor):
+        #validacion si no ingresa un numero
+        if False is isinstance(valor, (int, float)):
+            raise TypeError('Ingrese un numero entero valido')
+        if valor < 0:
+            raise ValueError('No es posible convertir distancias menores a 0')
+
+        print('Llamada al setter')
+        self._distancia = valor
+
+        print(self._distancia)
+
+    #funcion que convierte de km a millas
+    def convertir_a_km(self):
+        conversion = self.distancia * 1.609344
+        print(f'{self._distancia} millas convertidas a km son {conversion} km')
+
+if __name__ == '__main__':
+    avion = Millas()
+    avion.distancia  #el getter muestra cero ya que no le pasamos ningun dato
+    avion.distancia = 45
+    
+    avion.convertir_a_km() #se transforma la funcion
+    
