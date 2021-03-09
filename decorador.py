b14d6a43af375ed3778be9a47fa268755d51f09c
Obe Mauricio <mauricioobey@gmail.com>
2021-03-09 18:08:29 -0400

    whitout getter and setter v2

--- decorador.py
+++ decorador.py
@@ -1,4 +1,4 @@
-def funcion_decoradora(funcion):
+""" def funcion_decoradora(funcion):
     def wrapper():
         print('Este es el ultimo mensaje')
         funcion()
@@ -8,4 +8,18 @@ def funcion_decoradora(funcion):
 def zumbido():
     print('buzzzz')
 
-zumbido = funcion_decoradora(zumbido)
+zumbido = funcion_decoradora(zumbido) """
+
+#Clases whitout getters and setters
+class Millas:
+    def __init__(self, distancia = 0):
+        self.distancia = distancia
+
+    def convertir_a_kilometros(self):
+        return (self.distancia * 1.609344)
+
+avion = Millas()
+avion.distancia = 200
+print(avion.distancia)
+
+print(avion.convertir_a_kilometros())
